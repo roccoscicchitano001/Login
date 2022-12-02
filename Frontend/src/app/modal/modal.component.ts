4f6875999b48ca9dc659f9abb2fae1013faef810
Rocco <rocco.scicchitano@gpi.it>
2022-12-02 08:24:10 +0100

    wip modal

--- Frontend/src/app/modal/modal.component.ts
+++ Frontend/src/app/modal/modal.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { AccountService } from '@app/_services';
+import { FileService } from '@app/_services';
+import { UploadService } from '@app/_services';
 
 @Component({
   selector: 'app-modal',
@@ -8,5 +11,11 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 })
 
 export class ModalComponent {
-  constructor(public modalRef: MdbModalRef<ModalComponent>) {}
+
+  account = this.accountService.accountValue;
+  file: object;
+  upload: object;
+  accounts : any[];
+
+  constructor(public modalRef: MdbModalRef<ModalComponent>,private accountService: AccountService, private fileService:FileService,private uploadService:UploadService) {}
 }
