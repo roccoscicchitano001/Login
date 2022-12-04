65e3598cd53fa5a8cf7f6aaf8bda39452bbe5924
Rocco <rocco.scicchitano@gpi.it>
2022-12-04 19:22:40 +0100

    Upload del file RIUSCITO!

--- Frontend/src/app/modal/modal.component.ts
+++ Frontend/src/app/modal/modal.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 import { AccountService } from '@app/_services';
 import { FileService } from '@app/_services';
 import { UploadService } from '@app/_services';
-import { Upload } from '@app/_models';
-import { File } from '@app/_models';
+
 
 
 @Component({
@@ -16,53 +14,47 @@ import { File } from '@app/_models';
 })
 
 export class ModalComponent implements OnInit{
-  form: FormGroup;
-  formFile: FormGroup;
   account = this.accountService.accountValue;
   accounts : any[];
-  upload: Upload;
-  file: File;
+
+  fileToUpload: File | null = null;
 
 
-  constructor(public modalRef: MdbModalRef<ModalComponent>,private accountService: AccountService, private fileService:FileService,private uploadService:UploadService, private formBuilder: FormBuilder) {}
+  constructor(public modalRef: MdbModalRef<ModalComponent>,private accountService: AccountService, private fileService:FileService,private uploadService:UploadService) {}
 
   ngOnInit() {
     this.accountService.getAll()
         .pipe(first())
         .subscribe(accounts => this.accounts = accounts);
-    
-    this.form = this.formBuilder.group({
-      emailPaziente: [null, Validators.required],
-      file: [null, Validators.required]
-    });
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.form.controls; }
-
-  save(){
-    if (this.form.controls.emailPaziente.value==null&&this.form.controls.file.value==null){
-      return alert("Inserire elementi necessari!");
-    }
-
-    if (this.form.controls.emailPaziente.value==null){
-      return alert("E' necessario inserire l'utente di riferimento!");
-    }
-
-    if (this.form.controls.file.value==null){
-      return alert("E' necessario inserire il file da caricare!");
-    }
-
-    this.uploadFile();
-    
-    return this.modalRef.close();
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
   }
 
-  uploadFile(){
-    this.file=new File(this.form.controls.emailPaziente.value,this.account.email,"12222jjj",this.form.controls.file.value.name);
-    this.fileService.create(this.file).pipe(first());
-  }
+  postFile() {
+    let formData = new FormData();
+    formData.append("file", this.fileToUpload);
 
+    let req = new XMLHttpRequest();
+    req.open("POST", "http://localhost:8080/file/upload")
 
+    req.onload = function () {
+        console.log(req.responseText);
+
+        let response = req.responseText;
+
+        if (response !== null) {
+            let downloadUrl = "http://localhost:8080/file/download/" + response;
+            console.log(downloadUrl);
+        } else {
+            alert("Error Occured! No file returned");
+        }
+    }
+
+    req.send(formData);
+
+    this.modalRef.close();
+}
 
 }
